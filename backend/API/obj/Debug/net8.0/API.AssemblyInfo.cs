1e1331e8e8db57c2364aabc6ed955434d1a06eaa
Luis Mayayo <a25597@svalero.com>
2024-11-12 19:41:12 +0100

    frontend
    carpetas montadas y home empezada

--- backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a082a1364179398f3cda6879c01b2420c9c07ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92a2766d87ba3b493d01c2adb2111d3946ad40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
