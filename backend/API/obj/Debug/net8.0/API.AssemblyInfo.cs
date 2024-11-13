bb89cf4ef40e151d0097474b69eff932e91d4ac6
Luis Mayayo <a25597@svalero.com>
2024-11-13 16:29:46 +0100

    borrado de carpetas sobrantes

--- backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1331e8e8db57c2364aabc6ed955434d1a06eaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afefbd5a6fed9c84e7fd7a70dfd4feee507fcbe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
