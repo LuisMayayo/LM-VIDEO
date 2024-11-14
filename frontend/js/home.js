739ad3f9a7022018a01f2cff98889dba539695b3
MarcosLarraga04 <a25596@svalero.com>
2024-11-14 18:38:38 +0100

    header arreglado
    header arreglado

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
       menu.classList.toggle("is-active");
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.querySelector(".navbar__toggle");
+    const closeButton = document.querySelector(".navbar__close");
+    const menu = document.querySelector(".navbar__menu");
+  
+    toggleButton.addEventListener("click", () => {
+      menu.classList.add("is-active");
+    });
+  
+    closeButton.addEventListener("click", () => {
+      menu.classList.remove("is-active");
+    });
+  });
+  
+  
+  
+  
