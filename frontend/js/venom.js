ba8ed5d7b768f3b5bf3f46698b142d5da3cc1da2
MarcosLarraga04 <a25596@svalero.com>
2024-11-19 13:19:12 +0100

    terminar cartelera
    terminar cartelera

---
+++ frontend/js/venom.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Navbar Toggle
+    const toggleButton = document.querySelector(".navbar__toggle");
+    const closeButton = document.querySelector(".navbar__close");
+    const menu = document.querySelector(".navbar__menu");
+  
+    if (toggleButton) {
+      toggleButton.addEventListener("click", () => {
+        menu.classList.add("is-active");
+      });
+    }
+  
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        menu.classList.remove("is-active");
+      });
+    }
+  
